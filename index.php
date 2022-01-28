f411ed92e635b8c1f064c56745206db74244723d
Dark Angel <jonathanyombo@gmail.com>
2022-01-28 08:22:39 +0200

    Just a lol commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form action="action.php" method="post">
+        Email : <input type="email" name="email" /><br>
+        Mot de passe : <input type="password" name="password" /><br>
+        <button type="submit">Se connecter</button>
+    </form>
+</body>
+</html>
