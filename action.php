c2d95e58c58988a9976fb3874f22808202371542
Dark Angel <jonathanyombo@gmail.com>
2022-02-03 07:23:06 +0200

    Just a lol commit

--- action.php
+++ action.php
@@ -1,23 +1,7 @@
 <?php
-
-$etudiants = [
-    ['email' => '19kk192@esisalama.org', 'password' => '12345', 'nom' => 'Kitenge Kinkumba Michel'],
-    ['email' => '19ab023@esisalama.org', 'password' => 'azerty', 'nom' => 'Abekyamwale Baruani Jean'],
-    ['email' => '18ak057@esisalama.org', 'password' => 'mot de passe', 'nom' => 'Aristo Kazadi'],
-    ['email' => '18km231@esisalama.org', 'password' => 'pop123', 'nom' => 'Kilongo Mutatsh'],
-    ['email' => '15um358@esisalama.org', 'password' => 'jesus', 'nom' => 'Ubite Madeleine'],
-];
-
-foreach ($etudiants as $etudiant) {
-    if (($etudiant['email'] == $_POST['email']) && ($etudiant['password'] === $_POST['password'])) {
-        $utilisateur = $etudiant;
-    } else {
-        $utilisateur = '';
-    }
-}
-
-if ($utilisateur != '') {
-    echo "<h1>" . $utilisateur['nom'] . " - " . $utilisateur['email'] . "<h1/>";
-} else {
-    echo "<h1>Adresse mail ou mot de passe incorrect</h1>";
+$mail = $_POST['email'];
+$mdp = $_POST['mot_de_passe'];
+if ($mail = "" || $mdp = "" ) {
+    echo "un des champ est vide";
+    # code...
 }
--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
+    <title>Document</title>
 </head>
 <body>
     <form action="action.php" method="post">
-        Email : <input type="email" name="email" /><br>
-        Mot de passe : <input type="password" name="password" /><br>
-        <button type="submit">Se connecter</button>
+    email:<input type='email' name='email' placeholder='entrer votre email '>
+    mot de passe:<input type='password' name='mot_de_passe'placeholder='entrer votre mot de passe'>
+    <input type='submit' value='envoyer'>
+    
     </form>
+    
 </body>
 </html>
