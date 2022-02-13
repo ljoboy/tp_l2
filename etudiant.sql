da49def233d5ea8862dd2ad2ef7b66f7560772cb
Restyled.io <commits@restyled.io>
2022-02-13 18:45:15 +0000

    Restyled by pg_format

--- etudiant.sql
+++ etudiant.sql
@@ -8,69 +8,79 @@
 -- Version de PHP : 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 START TRANSACTION;
+
 SET time_zone = "+00:00";
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+
+/*!40101 SET NAMES utf8mb4 */
+;
 
 --
 -- Base de données : `esis_db`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Structure de la table `etudiant`
 --
 
 CREATE TABLE `etudiant` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(30) NOT NULL,
-  `postnom` varchar(30) NOT NULL,
-  `prenom` varchar(30) NOT NULL,
-  `genre` char(1) NOT NULL,
-  `matricule` char(7) NOT NULL,
-  `promotion` varchar(30) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `telephone` varchar(15) NOT NULL,
-  `adresse` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `id` int (11) NOT NULL,
+    `nom` varchar(30) NOT NULL,
+    `postnom` varchar(30) NOT NULL,
+    `prenom` varchar(30) NOT NULL,
+    `genre` char(1) NOT NULL,
+    `matricule` char(7) NOT NULL,
+    `promotion` varchar(30) NOT NULL,
+    `email` varchar(250) NOT NULL,
+    `telephone` varchar(15) NOT NULL,
+    `adresse` varchar(250) NOT NULL,
+    `password` varchar(250) NOT NULL) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 --
 -- Déchargement des données de la table `etudiant`
 --
 
-INSERT INTO `etudiant` (`id`, `nom`, `postnom`, `prenom`, `genre`, `matricule`, `promotion`, `email`, `telephone`, `adresse`, `password`) VALUES
-(41, 'ThÃ©o', 'Balick', 'ThÃ©ophile', 'm', '19TB490', 'Prepa', '19tb490@esisalama.org', '+243893666998', 'bel-airkampemba', 'good');
-
+INSERT INTO `etudiant` (`id`, `nom`, `postnom`, `prenom`, `genre`, `matricule`, `promotion`, `email`, `telephone`, `adresse`, `password`)
+        VALUES (41, 'ThÃ©o', 'Balick', 'ThÃ©ophile', 'm', '19TB490', 'Prepa', '19tb490@esisalama.org', '+243893666998', 'bel-airkampemba', 'good');
 --
 -- Index pour les tables déchargées
 --
-
 --
 -- Index pour la table `etudiant`
 --
+
 ALTER TABLE `etudiant`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `etudiant_matricule_uindex` (`matricule`),
-  ADD UNIQUE KEY `etudiant_telephone_uindex` (`telephone`);
+    ADD PRIMARY KEY (`id`),
+        ADD UNIQUE KEY `etudiant_matricule_uindex` (`matricule`),
+            ADD UNIQUE KEY `etudiant_telephone_uindex` (`telephone`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
-
 --
 -- AUTO_INCREMENT pour la table `etudiant`
 --
-ALTER TABLE `etudiant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+ALTER TABLE `etudiant` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 42;
+
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
+
