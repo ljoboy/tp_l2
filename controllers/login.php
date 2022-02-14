c7d7b98dfad95b9d26f79b118119059b12d7fe48
Restyled.io <commits@restyled.io>
2022-02-14 10:16:31 +0000

    Restyled by php-cs-fixer

--- controllers/login.php
+++ controllers/login.php
@@ -11,17 +11,16 @@ require 'functions.php';
 $db = db_connect();
 
 if (isset($_POST['email']) || isset($_POST['password'])) {
-    
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    if(empty($email)){
+    if (empty($email)) {
         header('Location: login.php?error=email is required');
         exit();
-    }else if(empty($password)){
+    } elseif (empty($password)) {
         header('Location: login.php?error=password is required');
         exit();
-    }else{
+    } else {
         $q = $db->prepare("SELECT * FROM esis_db.etudiant WHERE password = :password AND email = :email)");
 
         $q->execute([
@@ -30,14 +29,14 @@ if (isset($_POST['email']) || isset($_POST['password'])) {
         ]);
 
         $count = $statement->rowCount();
-        if($count > 0){
-        $_SESSION['auth'] = $email;
-        header('Location: index.php');
-        }else{
+        if ($count > 0) {
+            $_SESSION['auth'] = $email;
+            header('Location: index.php');
+        } else {
             $message = '<label>Email or Password incorrect</label>';
         }
     }
-}else{
+} else {
     header('Location: login.php');
     exit();
 }
