aae0bb6e6b60c6d2be554e9e6771b97b444903c8
josh-Muleshi <jmuleshi2@gmail.com>
2022-02-14 12:07:47 +0200

    add a login script to the project

--- controllers/login.php
+++ controllers/login.php
@@ -1,2 +1,37 @@
 <?php
+session_start();
 require 'functions.php';
+
+$db = db_connect();
+
+if (isset($_POST['email']) || isset($_POST['password'])) {
+    
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if(empty($email)){
+        header('Location: login.php?error=email is required');
+        exit();
+    }else if(empty($password)){
+        header('Location: login.php?error=password is required');
+        exit();
+    }else{
+        $q = $db->prepare("SELECT * FROM esis_db.etudiant WHERE password = :password AND email = :email)");
+
+        $q->execute([
+            'password' => $password,
+            'email' => $email
+        ]);
+
+        $count = $statement->rowCount();
+        if($count > 0){
+        $_SESSION['auth'] = $email;
+        header('Location: index.php');
+        }else{
+            $message = '<label>Email or Password incorrect</label>';
+        }
+    }
+}else{
+    header('Location: login.php');
+    exit();
+}
